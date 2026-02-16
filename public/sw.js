df30091a5cc69c6a5d807f2b6a4dea555610858a
Adrian Groh <adrian.groh@nordlb.de>
2026-02-16 01:03:10 +0100

    Release v1.2.1

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "todu-cache-v1.2.0";
+const CACHE_NAME = "todu-cache-v1.2.1";
 const OFFLINE_URLS = ["/", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", (event) => {
